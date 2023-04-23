063490776a464a7d7b3537f75dba8607506b4ff0
Jean Ricardo Vitório <112287680+JeanRVitorio@users.noreply.github.com>
2023-04-22 21:30:22 -0300

    Finalizado

---
+++ Jogo Da Forca (C)/Jogo da Forca/forca.h
@@ -0,0 +1,11 @@
+#define TAMANHO_PALAVRA 20
+
+void abertura();
+void escolhe_palavra();
+void add_palavra();
+void captura_chute();
+int busca_letra(char letra);
+void desenha_forca();
+int acertou();
+int chutes_errados();
+int enforcou();
--- Jogo Da Forca (C)/Jogo da Forca/jogo_da_forca.c
+++ Jogo Da Forca (C)/Jogo da Forca/jogo_da_forca.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+#include <stdlib.h>
 #include <windows.h>
+#include "forca.h"
 
-char palavrasecreta [20];
+char palavrasecreta [TAMANHO_PALAVRA];
 char letras[26];
-int tentativas = 0;
+int chutesdados = 0;
 
 void abertura() {
 
@@ -15,10 +18,27 @@ void abertura() {
 
 }
 
-void escolhe_palavra(){
-    
-    sprintf(palavrasecreta, "MELANCIA");
-    
+void escolhe_palavra() {
+	
+    FILE* f; // Define o tipo da variavel
+
+	f = fopen("palavras.txt", "r"); //Abri arquivo
+	if(f ==  0) {
+		printf("Banco de dados de palavras não disponível\n\n");
+		exit(1);
+	}
+
+	int qtddepalavras;
+	fscanf(f, "%d", &qtddepalavras); //Le quantidade de linhas no arquivo
+
+	srand(time(0));
+	int randomico = rand() % qtddepalavras; //Gera numero randomico
+
+	for(int i = 0; i <= randomico; i++) {
+		fscanf(f, "%s", palavrasecreta); //Define palavra aleatoria
+	}
+
+	fclose(f);
 }
 
 void captura_chute () {
@@ -27,16 +47,50 @@ void captura_chute () {
     printf("Qual letra? ");
     scanf(" %c", &chute);
 
-    letras[tentativas] = chute;
-    tentativas++;
+    letras[chutesdados] = chute;
+    chutesdados++;
 
+}
+
+void add_palavra(){
+    char quer;
+
+    printf("Deseja adicionar nova palavra? (S/N)");
+    scanf(" %c", &quer); //Captura caracter 
+
+    if (quer == 'S')
+    {
+        char nova_palavra;
+        printf("Qual a nova palavra? ");
+        scanf("%s", nova_palavra);
+
+        FILE* f;
+
+        f = fopen("palavra.txt", "r+"); // Abri Arquivo (r+ ler e escrever)
+        if(f ==  0) {
+		printf("Banco de dados de palavras não disponível\n\n");
+		exit(1);
+	}
+
+        int qtd;
+        fscanf(f, "%d", &qtd); //Le a qtd do arquivo
+        qtd++; // Adiciona na variavel
+
+        fseek(f, 0, SEEK_SET); // Altera a qtd de linhas no inicio do arquivo 
+        fprintf(f, "%d", qtd);
+
+        fseek(f, 0, SEEK_END); // Para inserir  nova palavra no final do arquivo
+        fprintf(f, "\n%s", nova_palavra); // Aqui adiciona realmente 
+        fclose(f);
+    }
+    
 }
 
 int busca_letra(char letra) {
 
     int achou_letra = 0;
 
-    for (int j = 0; j < tentativas; j++) // Verifica se a letra digitada está na palavrasecreta.
+    for (int j = 0; j < chutesdados; j++) // Verifica se a letra digitada está na palavrasecreta.
     {
         if (letras[j] == letra)
         {
@@ -51,7 +105,22 @@ int busca_letra(char letra) {
 
 void desenha_forca() {
 
-    printf("Você já deu %d chutes\n", tentativas);
+    int erros = chutes_errados();
+
+    printf("  _______       \n");
+    printf(" |/      |      \n");
+    printf(" |      %c%c%c  \n", (erros>=1?'(':' '), 
+        (erros>=1?'_':' '), (erros>=1?')':' '));
+    printf(" |      %c%c%c  \n", (erros>=3?'\\':' '), 
+        (erros>=2?'|':' '), (erros>=3?'/': ' '));
+    printf(" |       %c     \n", (erros>=2?'|':' '));
+    printf(" |      %c %c   \n", (erros>=4?'/':' '), 
+        (erros>=4?'\\':' '));
+    printf(" |              \n");
+    printf("_|___           \n");
+    printf("\n\n");
+
+    printf("Você já deu %d chutes\n", chutesdados);
 
     for (int i = 0; i < strlen(palavrasecreta); i++)
         {
@@ -71,15 +140,26 @@ void desenha_forca() {
 
 }
 
-int enforcou() {
+int acertou() {
+    for (int i = 0; i < strlen(palavrasecreta); i++) // Verifica a palavra secreta 
+    {
+        if (!busca_letra(palavrasecreta[i]) ) // Verifica se as letras da palavra secreta foi escolhida
+        {
+            return 0;
+        }
+        
+    }
+    return 1;
+}
 
+int chutes_errados(){
     int erros = 0;
 
-    for (int i = 0; i < tentativas; i++)
+    for (int i = 0; i < chutesdados; i++)
     {
         int existe = 0;
 
-        for (int j = 0; j < strlen(palavrasecreta); j++)
+        for (int j = 0; j < strlen (palavrasecreta); j++)
         {
             if ( letras[i] == palavrasecreta[j]){
                 existe = 1;
@@ -88,8 +168,11 @@ int enforcou() {
         }
         if (!existe) erros++;
     }
+}
 
-    return erros >= 5;
+int enforcou() {
+
+    return chutes_errados() >= 5;
 
 }
 
@@ -107,15 +190,24 @@ int main () {
     
     escolhe_palavra();  
 
-    int acertou = 0;
-
     do
     {
         desenha_forca();
         
         captura_chute();
 
-    } while ( !acertou && !enforcou() ); // ( acertou == 0 && enforcou == 0 )
+    } while ( !acertou() && !enforcou() ); // ( acertou == 0 && enforcou == 0 )
+
+    if (acertou())
+    {
+        printf("Parabéns\n");
+    } else {
+        printf("\n Perdeu\n");
+        printf("A palavra era ->%s<-\n\n", palavrasecreta);
+    }
+    
+
+    add_palavra();
     
     // Define codificação como sendo default
     SetConsoleOutputCP(CPAGE_DEFAULT);
